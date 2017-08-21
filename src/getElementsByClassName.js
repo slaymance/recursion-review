896ad275dfbdf200c31eb6d04410aac60f563220
Shane Laymance <shane.laymance@gmail.com>
2017-08-21 12:40:57 -0700

    stringifyJSON and getElementByClassName pass tests

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -6,5 +6,25 @@
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className
 ) {
+  console.log('getElementsByClassName called with className: ' + className );
   // your code here
+  var nodes = [];// define output array
+
+  var searchNodes = function(node){//define recurring function that takes in a node parameter
+    console.log('node.children.length: ' + node.children.length);
+    console.log('node.classList: ' + node.classList);
+    var classes = node.classList;//split classes by space and add to an array
+    if(classes.contains(className)){
+      nodes.push(node);
+    }//if class matches className, push node to ouput array
+  for (var i = 0; i < node.children.length; i++){//loop through array of classes
+
+    searchNodes(node.children[i])//call recurring function, with current node as input parameter
+  }//close loop
+  return;
+  }
+  searchNodes(document.body);
+  console.log(nodes);
+  return nodes;//return output array
+
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,39 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  if (obj === null) {
+    return 'null';
+  }
+  if (typeof obj === 'object') {
+    // return recursive function over object elements
+    if (Array.isArray(obj)) {
+      // recursion over each element in array
+
+      var string = [];
+      for (var i = 0; i < obj.length; i++) {
+        string.push(stringifyJSON(obj[i]));
+      }
+      return '[' + string.join(',') + ']';// return '[' + stringifyJson(element in array) + ']'
+    } else {
+      var string = [];
+      for (var key in obj) {
+        if(typeof(obj[key]) !== 'function' && obj[key] !== undefined){
+          string.push(stringifyJSON(key) + ':' + stringifyJSON(obj[key]));
+        }
+
+        //stringifyJSON(obj[key]);
+      }
+      return '{' + string.join(',') + '}'
+      // recursion over each key-value pair in object
+
+    }
+  } else if (typeof obj === 'string'){
+    return '"'+obj+'"';
+  } else if (typeof obj === 'function') {
+    return '';
+  } else {
+    return `${obj}`;
+  }
+
+  // return obj as a string
 };
