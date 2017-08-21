7c8e0eae52455b3b578d94813d5d84d651099095
Shane Laymance <shane.laymance@gmail.com>
2017-08-21 16:41:17 -0700

    object parseJSON works

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -4,4 +4,60 @@
 // but you're not, so you'll write it from scratch:
 var parseJSON = function(json) {
   // your code goes here
+  var jsonArray = json.split('');//split string split('')
+  //look at first character
+  if(jsonArray[0] === '{'){//object
+    jsonArray.shift();
+    jsonArray.pop();//takes off { & }
+    var object = {}
+    var index = 0;//temp index for either colon or comma
+
+      for (var i = 0; i < jsonArray.length; i++){
+        // var newArray = jsonArray.slice();
+        // console.log(JSON.stringify(newArray));
+        if(jsonArray[i] === ':'){//look for colon
+          var key = parseJSON(jsonArray.splice(0, i).join(''));
+          console.log('jsonArray before shift: ' + jsonArray);
+          jsonArray.shift();//takes off :
+            jsonArray.shift();//takes off space
+          console.log('jsonArray after key is spliced, and : is shifted: ' + jsonArray);
+          i = -1;
+          console.log('jsonArray.length : ' + jsonArray.length);
+          for(var j = 0; j < jsonArray.length; j++){
+            if(jsonArray[j] === ',' ){
+              var value = parseJSON(jsonArray.splice(0, j).join(''));
+              object[key] = value;
+              jsonArray.shift();//takes off comma
+              jsonArray.shift();//takes off space
+              console.log('jsonArray.length : ' + jsonArray.length);
+              break;
+            }
+            if(j === jsonArray.length - 1){
+              var value = parseJSON(jsonArray.splice(0, j + 1).join(''));
+              object[key] = value;
+              console.log('jsonArray.length : ' + jsonArray.length);
+              break;
+            }
+
+
+          }
+        }
+        //look for comma
+
+      }
+      console.log('object returned: ' + JSON.stringify(object));
+      return(object);
+
+  }else if(jsonArray[0] === '['){//array
+     var array = [];// define an empty array
+     
+  }else if(jsonArray[0] === '"'){
+    return(json);
+  }
+
+
+  //string
+  //null
+  //number
+  //undefined
 };
