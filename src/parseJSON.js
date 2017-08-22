0f7600f54c195849435f60138754576eec968f03
Shane Laymance <shane.laymance@gmail.com>
2017-08-22 14:23:43 -0700

    add further work on parseJSON

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -3,70 +3,60 @@
 
 // but you're not, so you'll write it from scratch:
 var parseJSON = function(json) {
-  // your code goes here
-  var jsonArray = json.split('');//split string split('')
-  //look at first character
-  if(jsonArray[0] === '{'){//object
-    jsonArray.shift();
-    jsonArray.pop();//takes off { & }
-    var object = {}
-    var index = 0;//temp index for either colon or comma
-
-      for (var i = 0; i < jsonArray.length; i++){
-        // var newArray = jsonArray.slice();
-        // console.log(JSON.stringify(newArray));
-        if(jsonArray[i] === ':'){//look for colon
-          var key = parseJSON(jsonArray.splice(0, i).join(''));
-          //console.log('jsonArray before shift: ' + jsonArray);
-          jsonArray.shift();//takes off :
-            jsonArray.shift();//takes off space
-          //console.log('jsonArray after key is spliced, and : is shifted: ' + jsonArray);
-          i = -1;
-          //console.log('jsonArray.length : ' + jsonArray.length);
-          for(var j = 0; j < jsonArray.length; j++){
-            if(jsonArray[j] === ',' ){
-              var value = parseJSON(jsonArray.splice(0, j).join(''));
-              object[key] = value;
-              jsonArray.shift();//takes off comma
-              jsonArray.shift();//takes off space
-              //console.log('jsonArray.length : ' + jsonArray.length);
-              break;
-            }
-            if(j === jsonArray.length - 1){
-              var value = parseJSON(jsonArray.splice(0, j + 1).join(''));
-              object[key] = value;
-              //console.log('jsonArray.length : ' + jsonArray.length);
-              break;
-            }
-
-
-          }
-        }
-        //look for comma
-
-      }
-      //console.log('object returned: ' + JSON.stringify(object));
-      return(object);
-
-  }else if(jsonArray[0] === '['){//array
-     var array = [];// define an empty array
-     //var arrayOfValues = json.split('');
-     jsonArray.shift();
-     jsonArray.pop();
-     var arrayValues = jsonArray.join('').split(',');
-     console.log('arrayValues: ' + JSON.stringify(arrayValues));
-     //var arrayOfValues2 = arrayOfValues1.split(',');
-     for(var i = 0; i < arrayValues.length; i++){
-       array.push(parseJSON(arrayValues[i]));
-     }
-     return(array);
-  }else if(jsonArray[0] === '"'){
-    return(json);
+  if (json === 'null') {
+    return null;
+  } else if (!isNaN(json)) {
+    return Number(json);
+  } else if (json === 'true') {
+    return true;
+  } else if (json === 'false') {
+    return false;
   }
 
+  var firstChar = json[0];
+  if (firstChar === '"') {
+    return json.substring(1, json.length - 1);
+  }
 
-  //string
-  //null
-  //number
-  //undefined
+  if (firstChar === '[') {
+    var array = [];
+    if (json.length > 2) {
+      var elementString = json.substring(1, json.length - 1);
+      var elementArray = elementString.split(', ');
+      if (elementArray.length === 1) { elementArray = elementString.split(',') }
+      for (var i = 0; i < elementArray.length; i++) {
+        array.push(parseJSON(elementArray[i]));
+      }
+    }
+    return array;
+
+  } else if (firstChar === '{') {
+    var obj = {};
+    if (json.length > 2) {
+      var elementString = json.substring(1, json.length - 1);
+      var elementArray = elementString.split(': ');
+      if (elementArray.length === 1) { elementArray = elementString.split(':') }
+      if (elementArray.length > 2) {
+        var tempObj = [];
+        debugger;
+        for (var i = 0; i < elementArray.length; i++) {
+          if (i % (elementArray.length - 1) === 0) {
+            tempObj.push(elementArray[i]);
+          } else {
+            for (var j = 1; j < elementArray[i].length; j++) {
+              if (elementArray[i][j] === '"') {
+                tempObj.push(elementArray[i].substring(0, j + 1), elementArray[i].substring(j + 3, elementArray[i].length));
+                break;
+              }
+            }
+          }
+        }
+        elementArray = tempObj;
+      }
+      for (var i = 1; i < elementArray.length; i += 2) {
+        obj[parseJSON(elementArray[i - 1])] = parseJSON(elementArray[i]);
+      }
+    }
+    return obj;
+  }
 };
